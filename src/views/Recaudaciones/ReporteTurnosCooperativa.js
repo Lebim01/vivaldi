5619c9f0d60178dcbbad140ec8fc1c4b1ec25796
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2019-07-05 11:00:12 -0600

    reporte boletos-tasas y turnos-cooperativas

---
+++ src/views/Recaudaciones/ReporteTurnosCooperativa.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import { ListPage, Card, CardBody, CardTitle, Label, FormGroup, Select, Input, Button } from './../../temeforest'
+import moment from 'moment'
+import { baseurl } from './../../utils/url'
+
+class ReporteTurnosCooperativa extends React.Component {
+    state = {
+        fecha_inicio : moment().format('YYYY-MM-DD'),
+        fecha_fin : moment().format('YYYY-MM-DD')
+    }
+    optionsCooperativa = {
+        url : `${baseurl}/cooperativa/`,
+        labelName: 'nombre',
+        valueName: 'id' 
+    }
+    optionsLocalidad = {
+        url : `${baseurl}/localidad/`,
+        labelName: 'nombre',
+        valueName: 'id' 
+    }
+    optionsDestino = {
+        url : `${baseurl}/ciudad/`,
+        labelName: 'nombre',
+        valueName: 'id' 
+    }
+    optionsFormaPago = [
+        {label:'Todas', value:''}
+    ]
+
+    onChange = name => (e) => {
+        this.setState({
+            [name]: e.target.value
+        })
+    }
+
+    buscar(){
+        this.setState({
+            refresh: true
+        })
+    }
+    
+    render(){
+        const { refresh } = this.state
+        return (
+            <div className="animated fadeIn">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <Card>
+                            <CardBody>
+                                <CardTitle>
+                                    Reporte de turnos por cooperativa
+                                </CardTitle>
+                                <br/>
+                                <div className="row">
+                                    <div className="col-sm-4">
+                                        <FormGroup className="row">
+                                            <Label className="col-sm-3">Cooperativa</Label>
+                                            <div className="col-sm-8">
+                                                <Select asyncOptions={this.optionsCooperativa} onChange={this.onChange('cooperativa')} value={this.state.cooperativa}/>
+                                            </div>
+                                        </FormGroup>
+                                        <FormGroup className="row">
+                                            <Label className="col-sm-3">Localidad</Label>
+                                            <div className="col-sm-8">
+                                                <Select asyncOptions={this.optionsLocalidad} onChange={this.onChange('localidad')} value={this.state.localidad}/>
+                                            </div>
+                                        </FormGroup>
+                                    </div>
+                                    <div className="col-sm-4">
+                                        <FormGroup className="row">
+                                            <Label className="col-sm-3">Fecha inicio</Label>
+                                            <div className="col-sm-8">
+                                                <Input type="date" onChange={this.onChange('fecha_inicio')} value={this.state.fecha_inicio} />
+                                            </div>
+                                        </FormGroup>
+                                        <FormGroup className="row">
+                                            <Label className="col-sm-3">Fecha fin</Label>
+                                            <div className="col-sm-8">
+                                                <Input type="date" onChange={this.onChange('fecha_fin')} value={this.state.fecha_fin} />
+                                            </div>
+                                        </FormGroup>
+                                    </div>
+                                    <div className="col-sm-4">
+                                        <FormGroup className="row">
+                                            <Label className="col-sm-3">Forma de pago</Label>
+                                            <div className="col-sm-8">
+                                                <Select options={this.optionsFormaPago} onChange={this.onChange('forma_pago')} value={this.state.forma_pago} />
+                                            </div>
+                                        </FormGroup>
+                                        <FormGroup className="row">
+                                            <Label className="col-sm-3">Destino</Label>
+                                            <div className="col-sm-8">
+                                                <Select asyncOptions={this.optionsDestino} onChange={this.onChange('destino')} value={this.state.destino} />
+                                            </div>
+                                        </FormGroup>
+                                    </div>
+                                </div>
+                                <ListPage
+                                    searchable={false}
+
+                                    fieldNames={['Boletero', 'Destino', 'Tipo', 'Cantidad', 'Valor unidad', 'Total']}
+                                    fields={['', '', '', '', '', '']}
+
+                                    url='recaudaciones/turnos_cooperativa'
+
+                                    menu='recaudaciones'
+                                    submenu='turnos-cooperativa'
+                                    parameters={this.state}
+                                    
+                                    history={this.props.history}
+                                    refresh={refresh}
+                                />
+                                <div className="row">
+                                    <div className="col-sm-12 text-center">
+                                        <Button type="success" style={{marginRight:5}}>Imprimir</Button>
+                                        <Button type="info" style={{marginLeft:5}}>Exportar</Button>
+                                    </div>
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ReporteTurnosCooperativa
