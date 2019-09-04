d72246ec9e1d36bee0922882293a016a7651deea
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2019-09-04 00:54:24 -0600

    tasas contingencia general

--- src/views/Reportes/ReporteTasasContingenciaGeneral.js
+++ src/views/Reportes/ReporteTasasContingenciaGeneral.js
@@ -99,10 +99,15 @@ class ReporteTasasContingenciaGeneral extends React.Component {
                                     searchable={false}
                                     
                                     head={[
-                                        [{colSpan:3,title:''},{colSpan:2,title:'Generado',style:style_text_center},{colSpan:2,title:'Asignado',style:style_text_center},{colSpan:2,title:'Saldo',style:style_text_center}],
+                                        [
+                                            {colSpan:3,title:''},
+                                            {colSpan:2,title:'Generado',style:style_text_center},
+                                            {colSpan:2,title:'Asignado',style:style_text_center},
+                                            {colSpan:2,title:'Saldo',style:style_text_center}
+                                        ],
                                         ['Fecha', 'Concepto', 'Cooperativa', 'Cantidad', 'Total', 'Cantidad', 'Total', 'Cantidad', 'Total']
                                     ]}
-                                    fields={['fecha', 'concepto', 'cooperativa_nombre', '', '', '', '' , '', '']}
+                                    fields={['fecha', 'concepto', 'cooperativa_nombre', 'generado_cantidad', 'generado_total', 'asignado_cantidad', 'asignado_total' , 'saldo_cantiddad', 'saldo_total']}
 
                                     endpoint='venta/tasa-contingencia-general'
                                     parameters={this.state}
