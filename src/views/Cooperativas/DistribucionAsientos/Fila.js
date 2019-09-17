c7b2b5599b54d48a9b6911b53d29cdb47432d683
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2019-09-17 17:19:03 -0600

    doc fila component

--- src/views/Cooperativas/DistribucionAsientos/Fila.js
+++ src/views/Cooperativas/DistribucionAsientos/Fila.js
@@ -3,18 +3,21 @@ import React from 'react'
 import DividerAsiento from './DividerAsiento'
 import Asiento from './Asiento'
 
+const lastHallSeats = 5
+const commonHallSeats = 4
+
 class Fila extends React.Component {
     render(){
 
         const { isLast, toggleActivate, asientos, asientos_desactivados, index } = this.props
 
         // quantity seats by hall
-        const cantidad_asientos_fila = isLast ? 5 :4
+        const cantidad_asientos_fila = isLast ? lastHallSeats : commonHallSeats
         // index become seat
-        const asiento_inicio = isLast ? 0 : ((index-1) * 4) + 5
+        const asiento_inicio = isLast ? 0 : ((index-1) * commonHallSeats) + lastHallSeats
         // seats object
-        const asientos_fila = asientos.slice(asiento_inicio, asiento_inicio+cantidad_asientos_fila)
-        
+        const asientos_fila = asientos.slice(asiento_inicio, asiento_inicio + cantidad_asientos_fila)
+
         const styles = isLast ? { marginTop: 10, marginBotton: 10 } : {}
 
         return (
