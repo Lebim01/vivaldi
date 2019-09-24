11545fcea19173ded3103ec40d5af9d7006fd85b
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2019-09-24 01:30:32 -0600

    regex input fix

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,5 +1,5 @@
-export const INTEGER = /^(\d*)$|^(\-\d*)$/
+export const INTEGER = /^(\d+)$|^(\-\d+)$/
 
-export const POSITIVE_NUMBER_DECIMAL = /^(\d*\.?\d*)$|^(\d*)$|^(\.\d*)$/
+export const POSITIVE_NUMBER_DECIMAL = /^(\d+\.?\d+)$|^(\d+)$|^(\.\d+)$/
 
-export const POSITIVE_NUMBER_INTEGER = /^(\d*)$/
+export const POSITIVE_NUMBER_INTEGER = /^(\d+)$/
