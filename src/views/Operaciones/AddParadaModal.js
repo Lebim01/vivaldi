8fcf3b6ae0508f595807e39194ed8492802dc1f0
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2019-10-31 22:03:12 -0700

    paradas rutas

--- src/views/Operaciones/AddParadaModal.js
+++ src/views/Operaciones/AddParadaModal.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
-import { Button, FormGroup, InputIcon, Label, FormValidate, RSelect } from 'temeforest'
+import { Modal, ModalBody, ModalFooter, ModalHeader, TabContent, TabPane } from 'reactstrap';
+import { Button, FormGroup, Label, FormValidate, RSelect, Tabs, InputIcon } from 'temeforest'
 import { baseurl, getResults } from 'utils/url'
 
 // positivos decimal
 const pattern = /[(\d+\.\d)|]/
-
 class AddParadaModal extends React.Component {
 
-    state = { errors: [], data: {}, ciudades: [] }
+    state = { errors: [], data: {}, ciudades: [], tipos_servicio: [], tipos_clientes: [], tab: "1" }
 
     optionsParada = {
         url : `${baseurl}/ciudad/`,
@@ -18,6 +17,53 @@ class AddParadaModal extends React.Component {
     
     componentDidMount(){
         this.loadOptionsCiudades()
+        this.getTiposServicios()
+        this.getTiposClientes()
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps.show !== this.props.show && this.props.show){
+            this.decodeTarifas()
+        }
+    }
+
+    decodeTarifas(){
+        const { tarifas } = this.state.data
+
+        let _tarifas = {}
+
+        for(let i in tarifas){
+            const { tipo_servicio, tipo_cliente, tarifa } = tarifas[i]
+
+            if(!_tarifas[tipo_servicio]) _tarifas[tipo_servicio] = {}
+
+            _tarifas[tipo_servicio][tipo_cliente] = tarifa
+        }
+
+        this.setState({
+            data : {
+                ...this.state.data,
+                tarifas : _tarifas
+            }
+        })
+    }
+
+    encodeTarifas(){
+        const { tarifas } = this.state.data
+
+        let _tarifas = []
+
+        for(let tipo_servicio in tarifas){
+            for(let tipo_cliente in tarifas[tipo_servicio]){
+                _tarifas.push({
+                    tipo_servicio,
+                    tipo_cliente,
+                    tarifa : tarifas[tipo_servicio][tipo_cliente]
+                })
+            }
+        }
+
+        return _tarifas
     }
 
     loadOptionsCiudades = async () => {
@@ -49,6 +95,18 @@ class AddParadaModal extends React.Component {
         })
     }
 
+    onChangeTarifa = (tipo_servicio, tipo_cliente) => e => {
+        let _data = { ...this.state.data }
+        
+        if(!_data.tarifas) _data.tarifas = {}
+        if(!_data.tarifas[tipo_servicio]) _data.tarifas[tipo_servicio] = {}
+        _data.tarifas[tipo_servicio][tipo_cliente] = e.target.value
+        
+        this.setState({
+            data : _data
+        })
+    }
+
     onSaveData = (name, value) => {
         let data = this.state.data
         data[name] = value
@@ -64,9 +122,21 @@ class AddParadaModal extends React.Component {
     }
 
     guardar = () => {
-        const { ciudad, ciudad_nombre, orden_llegada, tarifa_normal, tarifa_media, tarifa_ejecutiva_normal, tarifa_ejecutiva_media, index, id } = this.state.data
+        const { ciudad, ciudad_nombre, index, id } = this.state.data
+
+        if(!ciudad){
+            this.setState({
+                errors : [
+                    'ciudad'
+                ]
+            })
+            return
+        }
+
         if(this.props.guardar){
-            let _exito = this.props.guardar({ ciudad, ciudad_nombre, orden_llegada, tarifa_media, tarifa_normal, tarifa_ejecutiva_normal, tarifa_ejecutiva_media, index, id})
+            let _tarifas = this.encodeTarifas()
+
+            let _exito = this.props.guardar({ ciudad, ciudad_nombre, tarifas : _tarifas, index, id})
             if(_exito){
                 this.setState({
                     data : {},
@@ -80,8 +150,30 @@ class AddParadaModal extends React.Component {
         }
     }
 
+    changeTab = (tab) => {
+        this.setState({tab})
+    }
+
+    getTiposServicios = async () => {
+        const results = await getResults(`${baseurl}/tipoServicio/`, true)
+        this.setState({
+            tipos_servicio: results
+        })
+    }
+
+    getTiposClientes = async () => {
+        const results = await getResults(`${baseurl}/tipoCliente/`, true)
+        this.setState({
+            tipos_clientes: results
+        })
+    }
+
     render(){
-        const { errors } = this.state
+        const { errors, tipos_servicio, tipos_clientes, data } = this.state
+        const { tarifas } = data
+
+        const tabs = tipos_servicio.map((row) => ({ link: `${row.id}`, text: row.nombre }))
+
         return (
             <Modal isOpen={this.props.show} toggle={this.toggle}>
                 <ModalHeader toggle={this.toggle}>Agregar Parada</ModalHeader>
@@ -95,39 +187,40 @@ class AddParadaModal extends React.Component {
                         <FormGroup className="row">
                             <Label className="col-sm-4">Parada</Label>
                             <div className="col-sm-6">
-                                <RSelect asyncOptions={this.optionsParada} onChange={(value, label) => { this.onSaveData('ciudad', value); this.onSaveData('ciudad_nombre', label)}} value={this.state.data.ciudad} error={errors.includes('ciudad')} />
+                                <RSelect asyncOptions={this.optionsParada} onChange={(value, label) => { this.onSaveData('ciudad', value); this.onSaveData('ciudad_nombre', label)}} value={this.state.data.ciudad} />
+                                {errors.includes('ciudad') && <span className="text-danger">Este campo es requerido</span>}
                             </div>
                         </FormGroup>
-                        <FormGroup className="row">
-                            <Label className="col-sm-4">Llegada</Label>
-                            <div className="col-sm-6">
-                                <InputIcon icon={<span>#</span>} type="number" min="1" onChange={this.onChange('orden_llegada')} value={this.state.data.orden_llegada} error={errors.includes('orden_llegada')} />
-                            </div>
-                        </FormGroup>
-                        <FormGroup className="row">
-                            <Label className="col-sm-4">Tarifa Normal</Label>
-                            <div className="col-sm-6">
-                                <InputIcon icon={<i className="fas fa-dollar-sign"/>} type="number" step="0.1" onChange={this.onChange('tarifa_normal')} value={this.state.data.tarifa_normal} error={errors.includes('tarifa_normal')} />
-                            </div>
-                        </FormGroup>
-                        <FormGroup className="row">
-                            <Label className="col-sm-4">Tarifa Ejecutiva Normal</Label>
-                            <div className="col-sm-6">
-                                <InputIcon icon={<i className="fas fa-dollar-sign"/>} type="number" step="0.1" onChange={this.onChange('tarifa_ejecutiva_normal')} value={this.state.data.tarifa_ejecutiva_normal} error={errors.includes('tarifa_ejecutiva_normal')} />
-                            </div>
-                        </FormGroup>
-                        <FormGroup className="row">
-                            <Label className="col-sm-4">Tarifa Media</Label>
-                            <div className="col-sm-6">
-                                <InputIcon icon={<i className="fas fa-dollar-sign"/>} type="number" step="0.1" onChange={this.onChange('tarifa_media')} value={this.state.data.tarifa_media} error={errors.includes('tarifa_media')} />
-                            </div>
-                        </FormGroup>
-                        <FormGroup className="row">
-                            <Label className="col-sm-4">Tarifa Ejecutiva Media</Label>
-                            <div className="col-sm-6">
-                                <InputIcon icon={<i className="fas fa-dollar-sign"/>} type="number" step="0.1" onChange={this.onChange('tarifa_ejecutiva_media')} value={this.state.data.tarifa_ejecutiva_media} error={errors.includes('tarifa_ejecutiva_media')} />
-                            </div>
-                        </FormGroup>
+                        
+                        <Tabs tab={this.state.tab} tabs={tabs} onClickTab={this.changeTab}/>
+                        <TabContent activeTab={this.state.tab}>
+                            {tipos_servicio.map((tipo_servicio) => 
+                                <TabPane tabId={`${tipo_servicio.id}`}>
+                                    
+                                    <br/>
+                                    { tipos_clientes.map((tipo_cliente) => 
+                                        <FormGroup className="row">
+                                            <Label className="col-sm-4">{tipo_cliente.nombre}</Label>
+                                            <div className="col-sm-6">
+                                                <InputIcon
+                                                    icon={<i className="fas fa-dollar-sign"/>} 
+                                                    type="number" 
+                                                    step="0.1" 
+                                                    onChange={this.onChangeTarifa(tipo_servicio.id, tipo_cliente.id)} 
+                                                    value={
+                                                        tarifas && tarifas[tipo_servicio.id]
+                                                            ? tarifas[tipo_servicio.id][tipo_cliente.id]
+                                                            : ''
+                                                    } 
+                                                />
+                                            </div>
+                                        </FormGroup>
+                                    )}
+
+                                </TabPane>
+                            )}
+                        </TabContent>
+
                     </FormValidate>
                 </ModalBody>
                 <ModalFooter>
--- src/views/Operaciones/EditRutas.js
+++ src/views/Operaciones/EditRutas.js
@@ -27,11 +27,6 @@ class RecordRow extends React.Component {
                     </Button>{' '}
                     {this.props.ciudad_nombre}
                 </td>
-                <td>{this.props.orden_llegada}</td>
-                <td>{this.props.tarifa_normal}</td>
-                <td>{this.props.tarifa_ejecutiva_normal}</td>
-                <td>{this.props.tarifa_media}</td>
-                <td>{this.props.tarifa_ejecutiva_media}</td>
                 <td>
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id={name_input} name={name_input} checked={this.props.is_enable} onChange={this.props.onChange('is_enable')} />
@@ -210,11 +205,6 @@ class MainView extends React.Component {
                                 <thead>
                                     <tr>
                                         <th>Parada</th>
-                                        <th>Llegada</th>
-                                        <th>Tarifa Normal</th>
-                                        <th>Tarifa Ejecutiva Normal</th>
-                                        <th>Tarifa Media</th>
-                                        <th>Tarifa Ejecutiva Media</th>
                                         <th>Activa</th>
                                     </tr>
                                 </thead>
