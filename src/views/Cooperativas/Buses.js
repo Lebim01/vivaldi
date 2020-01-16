4431cf5a0880ca316ce60f3aa2fb391e07132067
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2020-01-16 16:53:15 -0700

    filtro estado de buses

--- src/views/Cooperativas/Buses.js
+++ src/views/Cooperativas/Buses.js
@@ -28,8 +28,8 @@ function Buses(props) {
     ]*/
 
    const optionsEstado = [
-        { value:0, label: 'Pendiente' },
-        { value:1, label: 'Aceptado' },
+        { value:'true', label: 'Habilitado' },
+        { value:'false', label: 'Deshabilitado' },
     ]
 
     const onChange = name => (e) => {
@@ -73,68 +73,65 @@ function Buses(props) {
                     <div className="col-sm-12">
                         <Card>
                             <CardBody>
-                            <div className="col-sm-12">
-                            <ListPage
+                                <div className="col-sm-12">
+                                    <ListPage
+                                        //exportExcel
+                                        imprimirPantalla
+                                        id="report"
+                                        key_permission= "bus"
 
+                                        title="Listado de Buses"
+                                        filtersZone={
 
-                            //exportExcel
-                            imprimirPantalla
-                            id="report"
-                            key_permission= "bus"
-
-                            //title="Listado de Buses"
-                            title="Listado de Buses"
-                            filtersZone={
-
-                                <div className="row">
-                                    <div className="col-sm-4">
-                                        <FormGroup className="row">
-                                            <Label className="col-sm-5">Cooperativa</Label>
-                                            <div className="col-sm-7">
-                                                <Select asyncOptions={optionsCooperativa} defaultOption="Todos" onChange={onChange('cooperativa')} value={state.cooperativa}/>
+                                            <div className="row">
+                                                <div className="col-sm-4">
+                                                    <FormGroup className="row">
+                                                        <Label className="col-sm-5">Cooperativa</Label>
+                                                        <div className="col-sm-7">
+                                                            <Select asyncOptions={optionsCooperativa} defaultOption="Todos" onChange={onChange('cooperativa')} value={state.cooperativa}/>
+                                                        </div>
+                                                    </FormGroup>
+                                                </div>
+                                                <div className="col-sm-4">
+                                                    <FormGroup className="row">
+                                                        <Label className="col-sm-5">Estado</Label>
+                                                        <div className="col-sm-7">
+                                                            <Select asyncOptions={optionsEstado} defaultOption="Todos" onChange={onChange('estado')} value={state.estado} /> 
+                                                        </div>
+                                                    </FormGroup>
+                                                </div>
                                             </div>
-                                        </FormGroup>
-                                    </div>
-                                    <div className="col-sm-4">
-                                        <FormGroup className="row">
-                                            <Label className="col-sm-5">Estado</Label>
-                                            <div className="col-sm-7">
-                                                <Select asyncOptions={optionsEstado} defaultOption="Todos" onChange={onChange('estado')} value={state.estado} /> 
-                                            </div>
-                                        </FormGroup>
-                                    </div>
-                                </div>
-                                }
+                                        }
 
 
-                                    searchable={true}
-                                    searchPlaceholder="Cooperativa, Placa, Disco"
+                                        searchable={true}
+                                        searchPlaceholder="Cooperativa, Placa, Disco"
 
-                                    //key_permission="bus"
+                                        fieldNames={['Cooperativa', 'Placa', 'Disco', 'Tipo', 'Capacidad', 'Dueño', 'Saldo']}
+                                        fields={[
+                                            'cooperativa_nombre', 'placa',
+                                            (row) => <span style={{textAlign:"right", position: 'relative', right:'-40%'}}>{row.disco}</span>,
+                                            'tipo_servicio_nombre',
+                                            (row) => <label style={{float:"center", position:"relative", left: "25%", fontWeight: 300}}>{row.capacidad}</label>,
+                                            'propietario_nombre',
+                                            (row) => <label style={{float:"center", position:"relative", left: "25%", fontWeight: 300}}>${moneyFormat(row.saldo)}</label>
+                                        ]}
 
-                                    fieldNames={['Cooperativa', 'Placa', 'Disco', 'Tipo', 'Capacidad', 'Dueño', 'Saldo']}
-                                    fields={['cooperativa_nombre', 'placa',
-                                    (row) => <span style={{textAlign:"right", position: 'relative', right:'-40%'}}>{row.disco}</span>,
-                                    'tipo_servicio_nombre',
-                                    (row) => <label style={{float:"center", position:"relative", left: "25%", fontWeight: 300}}>{row.capacidad}</label>,
-                                    'propietario_nombre',
-                                    (row) => <label style={{float:"center", position:"relative", left: "25%", fontWeight: 300}}>${moneyFormat(row.saldo)}</label>]}
+                                        endpoint='bus'
+                                        urlFront='cooperativas/buses'
 
-                                    endpoint='bus'
-                                    urlFront='cooperativas/buses'
+                                        exportExcel={exportExcel}
 
-                                    exportExcel={exportExcel}
-
-                                    history={props.history}
-                                    parameters={{
-                                                 ...state,
-                                                type: 'list'
-                                    }}
-                                    filters={{
-                                        persist: true,
-                                        callback: setState
-                                    }}
-                                />
+                                        history={props.history}
+                                        parameters={{
+                                            ...state,
+                                            type: 'list'
+                                        }}
+                                        filters={{
+                                            persist: true,
+                                            callback: setState
+                                        }}
+                                    />
                                 </div>
                             </CardBody>
                         </Card>
