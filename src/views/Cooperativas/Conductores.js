e39fcda6d59ce9397d917b007ee7a1ec7bed296f
Kevin Alvarado <kevinalva_777@hotmail.com>
2020-01-17 01:54:03 -0500

    filtros agregados por cooperativa en conductores y punto de venta

--- src/views/Cooperativas/Conductores.js
+++ src/views/Cooperativas/Conductores.js
@@ -1,42 +1,81 @@
-import React from 'react'
-import { Card, CardBody, ListPage, Permission } from 'temeforest'
+import React, { useState } from 'react'
+import { Card, CardBody, CardTitle, ListPage, FormGroup, Label, Select, Permission  } from 'temeforest'
+import { htmlToXls } from 'utils/exportData'
+import { baseurl } from 'utils/url'
+import { moneyFormat } from 'utils/number'
 
-class Conductores extends React.Component {
-    render(){
-        return (
-            <Permission key_permission="view_conductor" mode="redirect">
-                <div className="animated fadeIn">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <Card>
-                                <CardBody>
-                                    <ListPage
-                                        title="Listado de Conductores"
+function Conductores(props) {
 
-                                        key_permission="conductor"
+    const [state, setState] = useState({})
 
-                                        showStatus={true}
-                                        searchable={true}
-                                        searchPlaceholder="Cooperativa, Nombre, Identificación"
-
-                                        fieldNames={['Cooperativa', 'Nombre', 'Identificación']}
-                                        fields={['cooperativa_nombre', 'nombre', 'identificacion']}
-
-                                        endpoint='conductor'
-                                        urlFront='cooperativas/conductores'
-                                        history={this.props.history}
-                                        parameters={{
-                                            type: 'list'
-                                        }}
-                                    />
-                                </CardBody>
-                            </Card>
-                        </div>
-                    </div>
-                </div>
-            </Permission>
-        )
+    const optionsCooperativa = {
+        url : `${baseurl}/cooperativa/`,
+        labelName: 'nombre',
+        valueName: 'id'
     }
+
+    const onChange = name => (e) => {
+        setState({
+            [name]: e.target.value
+        })
+    }
+
+    return (
+        <Permission key_permission="view_conductor" mode="redirect">
+        <div className="animated fadeIn">
+            <div className="row">
+                <div className="col-sm-12">
+                    <Card>
+                        <CardBody>
+                            <ListPage
+                                exportExcel
+                                imprimirPantalla
+                                id="report"
+                                title="Listado de Conductores"
+
+                                key_permission="conductor"
+
+                                filtersZone = {
+                                    <div className="row">
+                                        <div className="col-sm-4">
+                                            <FormGroup className="row">
+                                                <Label className="col-sm-5">Cooperativa</Label>
+                                                <div className="col-sm-7">
+                                                    <Select asyncOptions={optionsCooperativa} defaultOption="Todos" onChange={onChange('cooperativa')} value={state.cooperativa}/>
+                                                </div>
+                                            </FormGroup>
+                                        </div>
+                                    </div>
+                                }
+
+                                searchable={true}
+                                searchPlaceholder="Cooperativa, Nombre, Identificación"
+
+                                fieldNames={['Cooperativa', 'Nombre', 'Identificación']}
+                                fields={['cooperativa_nombre', 'nombre', 'identificacion']}
+
+                                endpoint='conductor'
+                                urlFront='cooperativas/conductores'
+
+                                //exportExcel={exportExcel}
+
+                                history={props.history}
+                                parameters={{
+                                    ...state,
+                                    type: 'list'
+                                }}
+                                filters={{
+                                persist: true,
+                                callback: setState
+                                }}
+                            />
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    </Permission>
+    )
 }
 
 export default Conductores
--- src/views/Cooperativas/PuntoVenta.js
+++ src/views/Cooperativas/PuntoVenta.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { Card, CardBody, ListPage, Permission } from 'temeforest'
+import { Card, CardBody, CardTitle, ListPage, FormGroup, Label, Select, Permission  } from 'temeforest'
+import { htmlToXls } from 'utils/exportData'
+import { baseurl, objectToUrl } from 'utils/url'
+import { moneyFormat } from 'utils/number'
 
 class PuntoVenta extends React.Component {
 
-    renderCooperativas(row){
+    //const [state, setState] = useState({})
+    state = {}
+
+    optionsCooperativa = {
+        url : `${baseurl}/cooperativa/`,
+        labelName: 'nombre',
+        valueName: 'id' 
+    }
+
+    onChange = name => (e) => {
+        this.setState({
+            [name]: e.target.value
+        })
+    }
+
+    renderCooperativas= (row)=>{
         return (
             <>
                 { !row.externo &&
@@ -27,10 +45,11 @@ class PuntoVenta extends React.Component {
             </>
         )
     }
-
-    render(){
+ 
+    render() {
+        const { state } = this.state
         return (
-            <Permission key_permission="view_puntoventa" mode="redirect">
+        <Permission key_permission="view_puntoventa" mode="redirect">
                 <div className="animated fadeIn">
                     <div className="row">
                         <div className="col-sm-12">
@@ -38,18 +57,47 @@ class PuntoVenta extends React.Component {
                                 <CardBody>
                                     <ListPage
                                         title="Listado de Punto de venta"
-
+                                        exportExcel
+                                        imprimirPantalla
+                                        id="puntoventa"
                                         key_permission="puntoventa"
+
+                                        filtersZone = {
+                                            <div className="row">
+                                                <div className="col-sm-4">
+                                                    <FormGroup className="row">
+                                                        <Label className="col-sm-5">Cooperativa</Label>
+                                                        <div className="col-sm-7">
+                                                            <Select asyncOptions={this.optionsCooperativa} defaultOption="Todos" onChange={this.onChange('cooperativa')} value={this.state.cooperativa}/>
+                                                        </div>
+                                                    </FormGroup>
+                                                </div>
+                                            </div>
+                                        }
                                         
                                         showStatus={true}
                                         searchable={true}
                                         searchPlaceholder="Descripción, Cooperativa, Localidad"
 
                                         fieldNames={['Nombre', 'Cooperativa', 'Localidad']}
-                                        fields={['descripcion', this.renderCooperativas /*'cooperativa_nombre'*/, 'localidad_nombre']}
+                                        fields={['descripcion', this.renderCooperativas/* 'cooperativa_nombre', */,'localidad_nombre']}
 
                                         endpoint='venta/puntoventa'
                                         urlFront='cooperativas/punto-venta'
+
+                                        //exportExcel={exportExcel}
+
+                                        
+                                        parameters={
+                                            this.state
+                                          }
+
+                                        filters={{
+                                            persist: true,
+                                            callback: (parameters) => {
+                                                this.setState(parameters)
+                                            }
+                                        }}
                                         history={this.props.history}
                                     />
                                 </CardBody>
@@ -61,5 +109,6 @@ class PuntoVenta extends React.Component {
         )
     }
 }
+
 
 export default PuntoVenta
