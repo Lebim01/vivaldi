e5ffb7bde737132871cf7ffe393e5cc79e43e4c6
Kevin Alvarado <kevinalva_777@hotmail.com>
2020-01-20 18:52:43 -0500

    viajes planificados color arreglo

--- src/views/Operaciones/ViajesPlanificados.js
+++ src/views/Operaciones/ViajesPlanificados.js
@@ -201,7 +201,7 @@ class ViajesPlanificados extends React.Component {
                                                 if(row.conductor_puntos === 0) return 'bg-danger'
                                                 if(row.saldo === 0) return 'bg-orange-300'
                                                 if(ya_salio) return 'bg-info'
-                                                return ''
+                                                return 'bg-info'
                                             }}
                                             endpoint='venta/viajes-planificados'
                                             parameters={this.state.data}
