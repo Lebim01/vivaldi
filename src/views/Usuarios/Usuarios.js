f5a67b9aab011bf962c9cfea94c983c30cda653d
Kevin Alvarado <kevinalva_777@hotmail.com>
2020-01-20 11:45:49 -0500

    permisos a usuarios

--- src/views/Usuarios/Usuarios.js
+++ src/views/Usuarios/Usuarios.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Card, CardBody, CardTitle, ListPage, FormGroup, Label, Select, Permission  } from 'temeforest'
+import { Card, CardBody, CardTitle, ListPage, FormGroup, Label, Select, Permission, ReportPage  } from 'temeforest'
 import { htmlToXls } from 'utils/exportData'
 import { baseurl } from 'utils/url'
 import { moneyFormat } from 'utils/number'
@@ -27,77 +27,69 @@ function Usuarios(props) {
     }
 
     return (
+    <ReportPage printButtons={true} timestamp={false}>
         
-        <div className="animated fadeIn">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <Card>
-                            <CardBody>
-                                <ListPage
-                                    title="Listado de Usuarios"
+        <ListPage
+            title="Listado de Usuarios"
 
-                                    key_permission="users"
-                                    //exportExcel
-                                    //imprimirPantalla
-                                    //id="report"
-                               
-                                    filtersZone = {
-                                    <div className="row">
-                                        <div className="col-sm-4">
-                                            <FormGroup className="row">
-                                                <Label className="col-sm-5">Cooperativa</Label>
-                                                <div className="col-sm-7">
-                                                    <Select asyncOptions={optionsCooperativa} defaultOption="Todos" onChange={onChange('cooperativa')} value={state.cooperativa}/>
-                                                </div>
-                                            </FormGroup>
-                                        </div>
-                                        <div className="col-sm-4">
-                                            <FormGroup className="row">
-                                                <Label className="col-sm-5">Estado</Label>
-                                                <div className="col-sm-7">
-                                                    <Select options={optionsEstado} defaultOption="Todos" onChange={onChange('status')} value={state.status} /> 
-                                                </div>
-                                            </FormGroup>
-                                        </div>
-                                    </div>
-                                    }
-                                
-
-
-                                    showStatus={true}
-                                    searchable={true}
-                                    searchPlaceholder="Usuario, Nombre"
-
-                                    fieldNames={['Usuario', 'Nombre', 'Cooperativas']}
-                                    fields={['username', 'first_name', (row) => 
-                                        <ul>
-                                            { row.roles_cooperativa.map((r, i) => 
-                                                <li key={i}>{r.cooperativa_nombre}</li>
-                                            )}
-                                        </ul>
-                                    ]}
-
-                                    endpoint='usuario'
-                                    urlFront='usuarios/usuarios'
-    
-                                    //exportExcel={exportExcel}
-    
-                                    history={props.history}
-                                    parameters={{
-                                        ...state,
-                                        type: 'list'
-                                    }}
-                                    filters={{
-                                    persist: true,
-                                    callback: setState
-                                    }}
-                                />
-                            </CardBody>
-                        </Card>
-                    </div>
+            key_permission="user"
+            exportExcel
+            imprimirPantalla
+            id="usuario"
+        
+            filtersZone = {
+            <div className="row">
+                <div className="col-sm-4">
+                    <FormGroup className="row">
+                        <Label className="col-sm-5">Cooperativa</Label>
+                        <div className="col-sm-7">
+                            <Select asyncOptions={optionsCooperativa} defaultOption="Todos" onChange={onChange('cooperativa')} value={state.cooperativa}/>
+                        </div>
+                    </FormGroup>
+                </div>
+                <div className="col-sm-4">
+                    <FormGroup className="row">
+                        <Label className="col-sm-5">Estado</Label>
+                        <div className="col-sm-7">
+                            <Select options={optionsEstado} defaultOption="Todos" onChange={onChange('status')} value={state.status} /> 
+                        </div>
+                    </FormGroup>
                 </div>
             </div>
-    
+            }
+        
+
+
+            showStatus={true}
+            searchable={true}
+            searchPlaceholder="Usuario, Nombre"
+
+            fieldNames={['Usuario', 'Nombre', 'Cooperativas']}
+            fields={['username', 'first_name', (row) => 
+                <ul>
+                    { row.roles_cooperativa.map((r, i) => 
+                        <li key={i}>{r.cooperativa_nombre}</li>
+                    )}
+                </ul>
+            ]}
+
+            endpoint='usuario'
+            urlFront='usuarios/usuarios'
+
+            //exportExcel={exportExcel}
+
+            history={props.history}
+            parameters={{
+                ...state,
+                type: 'list'
+            }}
+            filters={{
+            persist: true,
+            callback: setState
+            }}
+        />
+                    
+        </ReportPage>
     )
 }
 
