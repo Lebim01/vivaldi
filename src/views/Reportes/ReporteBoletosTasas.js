395490c7a9cfebcb2cd5530f6f0fb3f0a5a6540a
Kevin Alvarado <kevinalvapearson777@gmail.com>
2020-02-11 15:47:40 -0500

    alinear en boletos tasas

--- src/views/Reportes/ReporteBoletosTasas.js
+++ src/views/Reportes/ReporteBoletosTasas.js
@@ -212,19 +212,19 @@ class ReporteBoletosTasas extends React.Component {
                                                 head={[['Tipo',
                                                 {
                                                     title:'Cantidad', 
-                                                    style:{textAlign:"right", position: 'relative', right:'50%' }
+                                                    style:{textAlign:"right", position: 'relative', right:'-20%' }
                                                 }, 
                                                 {
                                                     title: 'Cantidad anulado', 
-                                                    style:{textAlign:"right", position: 'relative', right:'50%' }
+                                                    style:{textAlign:"right", position: 'relative', right:'-20%' }
                                                 },
                                                 {
                                                     title: 'Total boleto',
-                                                    style:{textAlign:"right", position: 'relative', right:'50%' }
+                                                    style:{textAlign:"right", position: 'relative', right:'-20%' }
                                                 },
                                                 {
                                                     title: 'Total tasa', 
-                                                    style:{textAlign:"right", position: 'relative', right:'50%' }
+                                                    style:{textAlign:"right", position: 'relative', right:'-20%' }
                                                 }
                                                 ]]}
                                                 fields={[
