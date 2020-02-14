9115f1415a52e4f900589de979f4550ca8ac738e
Kevin Alvarado <kevinalvapearson777@gmail.com>
2020-02-14 14:25:04 -0500

    corrección combo box por cooperativa

--- src/views/Auditoria/RegistroAccion.js
+++ src/views/Auditoria/RegistroAccion.js
@@ -51,7 +51,8 @@ class RegistroAccion extends React.Component {
                                         <FormGroup className="row">
                                             <Label className="col-sm-5">Cooperativa</Label>
                                             <div className="col-sm-6">
-                                                <Select readOnly={this.state.tabla !== 'cooperativa'} onChange={this.onChange('object_id')} asyncOptions={this.optionsCooperativa} defaultOption="Todos" value={this.state.cooperativa}/>
+                                                
+                                                <Select asyncOptions={this.optionsCooperativa} defaultOption="Todos" onChange={this.onChange('cooperativa')} value={this.state.cooperativa}/>
                                             </div>
                                         </FormGroup>
                                         <FormGroup className="row">
