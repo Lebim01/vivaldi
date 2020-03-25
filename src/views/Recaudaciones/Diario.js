fe5f342c4edae9bba82cd296ef8f94d2ddc41e41
Kevin Alvarado <kevinalvapearson777@gmail.com>
2020-03-25 15:35:22 -0500

    filtro correccion

--- src/views/Recaudaciones/Diario.js
+++ src/views/Recaudaciones/Diario.js
@@ -34,7 +34,7 @@ class Diario extends React.Component {
         {value:'t', label:'Todos'},
         {value:'p', label:'Pendientes'},
         {value:'c', label:'Cobrados'},
-        {value:'v', label:'Vendidos'},
+        //{value:'v', label:'Vendidos'},
     ]
 
     onChange = name => (e) => {
