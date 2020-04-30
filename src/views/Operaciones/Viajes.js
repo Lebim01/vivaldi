cee693775f5fcc3b372a700d4051bc705f612735
Kevin Alvarado <kevinalvapearson777@gmail.com>
2020-04-30 14:07:59 -0500

    operaciones viajes columnas fecha creación y usuario creación

--- src/views/Operaciones/Viajes.js
+++ src/views/Operaciones/Viajes.js
@@ -153,10 +153,10 @@ class Viajes extends React.Component {
                                             }
 
                                             searchable={false}
-                                            fieldNames={['Fecha', 'Frecuencia','Hora salida', 'Pasajeros','Cooperativa', 'Disco', 'Ruta', 'Usuario creación', 'Fecha creación']}
+                                            fieldNames={['Fecha', 'Frecuencia','Hora salida', 'Pasajeros','Cooperativa', 'Disco', 'Ruta']}
                                             fields={['fecha','frecuencia_nombre', 'hora_salida', 'pasajeros', 'cooperativa_nombre',
                                             (row) => <span style={{textAlign:"right", position: 'relative', right:'-5%'}}>{row.bus_disco}</span>,
-                                            'ruta_nombre', 'usuario_creacion_username', 'creacion',]}
+                                            'ruta_nombre']}
 
                                             endpoint='viaje'
                                             urlFront={'operaciones/viajes'}
