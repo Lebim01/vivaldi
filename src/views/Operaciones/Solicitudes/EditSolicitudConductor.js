28dfad00b2f497b00c0a082849c5fb41589dfce2
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2020-05-12 14:20:56 -0600

    tipo de solicitud nombre

--- src/views/Operaciones/Solicitudes/EditSolicitudConductor.js
+++ src/views/Operaciones/Solicitudes/EditSolicitudConductor.js
@@ -38,7 +38,7 @@ class MainView extends React.Component {
                     <FormGroup className="row">
                         <Label className="col-sm-3">Tipo solicitud</Label>
                         <div className="col-sm-5">
-                            <Input value={this.props.tipo_solicitud} readOnly />
+                            <Input value={this.props.tipo_solicitud_nombre} readOnly />
                         </div>
                     </FormGroup>
                     <FormGroup className="row">
